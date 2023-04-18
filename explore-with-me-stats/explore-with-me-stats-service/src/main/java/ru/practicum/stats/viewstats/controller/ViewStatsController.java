14d96a6666b081809c2ca7beb5f0c3c8b843f5e5
Matvey Smorodin <mtv.smorodin@gmail.com>
2023-04-18 11:56:45 +0300

    refactor(ViewStatsController)

--- explore-with-me-stats/explore-with-me-stats-service/src/main/java/ru/practicum/stats/viewstats/controller/ViewStatsController.java
+++ explore-with-me-stats/explore-with-me-stats-service/src/main/java/ru/practicum/stats/viewstats/controller/ViewStatsController.java
@@ -2,13 +2,14 @@ package ru.practicum.stats.viewstats.controller;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.RequiredArgsConstructor;
+import org.apache.logging.log4j.util.Strings;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import ru.practicum.stats.viewstats.service.ViewStatsService;
 import ru.practicum.ewm.stats.dto.viewstats.ViewStats;
 import ru.practicum.ewm.stats.dto.viewstats.ViewStatsRequestDto;
+import ru.practicum.stats.viewstats.service.ViewStatsService;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -18,17 +19,18 @@ import java.util.List;
 @RequestMapping("/stats")
 public class ViewStatsController {
     private final ViewStatsService viewStatsService;
+    private static final String DATETIME_FORMAT_PATTERN = "yyyy-MM-dd HH:mm:ss";
 
     @GetMapping
     public List<ViewStats> viewStats(
             @RequestParam(name = "start")
-            @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+            @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = DATETIME_FORMAT_PATTERN)
             LocalDateTime start,
             @RequestParam(name = "end")
-            @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+            @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = DATETIME_FORMAT_PATTERN)
             LocalDateTime end,
             @RequestParam(name = "uris",
-                    required = false)
+                    defaultValue = Strings.EMPTY)
             List<String> uris,
             @RequestParam(name = "unique",
                     defaultValue = "false")
@@ -41,7 +43,7 @@ public class ViewStatsController {
                 .uris(uris)
                 .unique(unique)
                 .build();
-        return viewStatsService.viewStats(requestDto);
+        return viewStatsService.getViewStats(requestDto);
     }
 
 

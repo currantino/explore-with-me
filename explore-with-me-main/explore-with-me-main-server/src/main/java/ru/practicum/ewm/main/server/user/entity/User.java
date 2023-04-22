c3a94b98e66715e116da432a5699877df33b14b7
Matvey Smorodin <mtv.smorodin@gmail.com>
2023-04-22 13:36:48 +0300

    Add User entity

---
+++ explore-with-me-main/explore-with-me-main-server/src/main/java/ru/practicum/ewm/main/server/user/entity/User.java
@@ -0,0 +1,21 @@
+package ru.practicum.ewm.main.server.user.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "username")
+    private String name;
+}

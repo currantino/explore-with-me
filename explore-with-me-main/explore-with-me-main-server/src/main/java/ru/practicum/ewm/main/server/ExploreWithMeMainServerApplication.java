47ddb797fe62f8e91a4666dadfc5881000c466b9
Matvey Smorodin <mtv.smorodin@gmail.com>
2023-04-22 13:36:10 +0300

    Add main service application runner

---
+++ explore-with-me-main/explore-with-me-main-server/src/main/java/ru/practicum/ewm/main/server/ExploreWithMeMainServerApplication.java
@@ -0,0 +1,11 @@
+package ru.practicum.ewm.main.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExploreWithMeMainServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ExploreWithMeMainServerApplication.class, args);
+    }
+}

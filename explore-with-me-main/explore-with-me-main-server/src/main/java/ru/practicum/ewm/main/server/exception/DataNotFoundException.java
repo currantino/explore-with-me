6c515177501d26b1c5f358eda62641634a5068f6
Matvey Smorodin <mtv.smorodin@gmail.com>
2023-04-25 12:51:44 +0300

    Add timestamp to DataNotFoundException

--- explore-with-me-main/explore-with-me-main-server/src/main/java/ru/practicum/ewm/main/server/exception/DataNotFoundException.java
+++ explore-with-me-main/explore-with-me-main-server/src/main/java/ru/practicum/ewm/main/server/exception/DataNotFoundException.java
@@ -1,11 +1,19 @@
 package ru.practicum.ewm.main.server.exception;
 
+import lombok.Getter;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import java.time.LocalDateTime;
+
+@Getter
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class DataNotFoundException extends RuntimeException {
+    private final LocalDateTime timestamp;
+
     public DataNotFoundException(String message) {
         super(message);
+        this.timestamp = LocalDateTime.now();
     }
+
 }

4001f9e67effb49b5377ba87c30f440d1336a548
Matvey Smorodin <mtv.smorodin@gmail.com>
2023-04-29 18:49:05 +0300

    Fix Location

--- explore-with-me-main/explore-with-me-main-server/src/main/java/ru/practicum/ewm/main/server/location/entity/Location.java
+++ explore-with-me-main/explore-with-me-main-server/src/main/java/ru/practicum/ewm/main/server/location/entity/Location.java
@@ -1,12 +1,17 @@
 package ru.practicum.ewm.main.server.location.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 
 @Entity
 @Table(name = "location")
+@Getter
+@Setter
 public class Location {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "location_id")
     private Long id;
     @Column(name = "latitude")
